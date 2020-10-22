a715363b4551875bcd75c8586ca01d90a2233cc8
Hadeer <mhadeer95@yahoo.com>
2020-10-22 12:16:19 +0200

    solve login issues ui finished

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -28,7 +28,10 @@ class _SignInState extends State<SignIn> with SingleTickerProviderStateMixin {
 
   TextEditingController emailTextEditingController = TextEditingController();
   TextEditingController passwordTextEditingController = TextEditingController();
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
   final formKey = GlobalKey<FormState>();
+  final FocusNode _nameFocus = FocusNode();
+  final FocusNode _passwordFocus = FocusNode();
   bool switchState = true;
   var mainColor = Color(0xFF1589d2);
   var height, width;
@@ -81,39 +84,50 @@ class _SignInState extends State<SignIn> with SingleTickerProviderStateMixin {
     height = MediaQuery.of(context).size.height;
     width = MediaQuery.of(context).size.width;
     return Scaffold(
+      key: scaffoldKey,
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: Stack(
-          children: <Widget>[
-            SlideTransition(
-              position: _logoSlideAnimation,
-              child: ScaleTransition(
-                scale: _logoScaleAnimation,
-                child: buildLogo(),
-              ),
-            ),
-            SlideTransition(
-              position: _formContainerAnimation,
-              child: Container(
-                margin:
-                EdgeInsets.only(top: 240, left: 20, right: 20, bottom: 50),
-                width: width,
-                height: height * 0.6,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black12,
-                      blurRadius: 10,
-                      offset: Offset(1.0, 4.0),
-                    ),
-                  ],
+        child: Container(
+          width: width ,
+            height: height,
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: SlideTransition(
+                  position: _logoSlideAnimation,
+                  child: ScaleTransition(
+                    scale: _logoScaleAnimation,
+                    child: buildLogo(),
+                  ),
                 ),
-                child: buildLoginForm(),
               ),
-            ),
-          ],
+              Expanded(
+                flex: 4,
+                child: SlideTransition(
+                  position: _formContainerAnimation,
+                  child: Container(
+                    margin:
+                    EdgeInsets.only( left: 20, right: 20, bottom: 50),
+                    width: width,
+                    height: height * 0.6,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 10,
+                          offset: Offset(1.0, 4.0),
+                        ),
+                      ],
+                    ),
+                    child: buildLoginForm(),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -148,7 +162,8 @@ class _SignInState extends State<SignIn> with SingleTickerProviderStateMixin {
       margin: EdgeInsets.all(20.0),
       child: Form(
         key: formKey,
-        child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+        child: Column(
+            mainAxisSize: MainAxisSize.min, children: <Widget>[
           BlocListener<LoginBloc, BaseResultState>(
             bloc: _bloc,
             listener: (context, state) async {
@@ -184,21 +199,21 @@ class _SignInState extends State<SignIn> with SingleTickerProviderStateMixin {
             child: Container(),
           ),
           buildUserName(),
-          SizedBox(
-            height: 10,
-          ),
+          // SizedBox(
+          //   height: 10,
+          // ),
           buildPassword(),
-          SizedBox(
-            height: 10,
-          ),
+          // SizedBox(
+          //   height: 10,
+          // ),
           buildKeepMeLogIn(),
-          SizedBox(
-            height: 10,
-          ),
+          // SizedBox(
+          //   height: 10,
+          // ),
           buildMacAddress(),
-          SizedBox(
-            height: 10,
-          ),
+          // SizedBox(
+          //   height: 10,
+          // ),
           buildLogInButton(),
         ]),
       ),
@@ -206,53 +221,65 @@ class _SignInState extends State<SignIn> with SingleTickerProviderStateMixin {
   }
 
   Widget buildUserName() {
-    return Container(
-      margin: EdgeInsets.only(top: 10, bottom: 10),
-      padding: EdgeInsets.only(left: 10.0, right: 10.0),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(
-            color: Colors.blue,
-          ),
-          borderRadius: BorderRadius.all(Radius.circular(10))),
-      child: TextFormField(
-          controller: emailTextEditingController,
-          textAlign: TextAlign.start,
-          style: TextStyle(color: Colors.blue[50]),
-          decoration: InputDecoration(
-            prefixIcon: Icon(
-              Icons.account_circle,
-              color: Colors.grey[700],
+    return Expanded(
+      flex: 2,
+      child: Container(
+        margin: EdgeInsets.only(top: 10, bottom: 10),
+        padding: EdgeInsets.only(left: 10.0, right: 10.0),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(
+              color:Colors.blue[300],
             ),
-            labelText: 'User Name',
-            labelStyle: TextStyle(color: Colors.grey[700], letterSpacing: 1.0),
-            border: InputBorder.none,
-          ),
-          validator: (val) {
-            return val.length < 4 ? "Enter a Valid username" : null;
-          }),
+            borderRadius: BorderRadius.all(Radius.circular(10))),
+        child: TextFormField(
+            controller: emailTextEditingController,
+            textInputAction: TextInputAction.next,
+            textAlign: TextAlign.start,
+            style: TextStyle(color: Colors.blue),
+            decoration: InputDecoration(
+              prefixIcon: Icon(
+                Icons.account_circle,
+                color: Colors.grey[700],
+              ),
+              labelText: 'User Name',
+              labelStyle: TextStyle(color: Colors.grey[700], letterSpacing: 1.0),
+              border: InputBorder.none,
+            ),
+            validator: (val) {
+              return val.length < 4 ? "Enter a Valid username" : null;
+            } ,
+          focusNode: _nameFocus,
+          onFieldSubmitted: (term){
+            _fieldFocusChange(context, _nameFocus, _passwordFocus);
+          },
+        ),
+      ),
     );
   }
 
   Widget buildMacAddress() {
-    return Container(
-        margin: EdgeInsets.only(left: 10.0, right: 10.0),
-        child: Align(
-          child: Row(
-            children: <Widget>[
-              Text(
-                'Mac Address :',
-                style: TextStyle(
-                    color: Colors.black87, fontWeight: FontWeight.normal),
-              ),
-              Text(
-                _platformVersion,
-                style:
-                TextStyle(color: mainColor, fontWeight: FontWeight.normal),
-              )
-            ],
-          ),
-        ));
+    return Expanded(
+      flex: 1,
+      child: Container(
+          margin: EdgeInsets.only(left: 10.0, right: 10.0),
+          child: Align(
+            child: Row(
+              children: <Widget>[
+                Text(
+                  'Mac Address :',
+                  style: TextStyle(
+                      color: Colors.black87, fontWeight: FontWeight.normal),
+                ),
+                Text(
+                  _platformVersion,
+                  style:
+                  TextStyle(color: mainColor, fontWeight: FontWeight.normal),
+                )
+              ],
+            ),
+          )),
+    );
   }
 
   void dismissLoading() async {
@@ -262,74 +289,85 @@ class _SignInState extends State<SignIn> with SingleTickerProviderStateMixin {
   }
 
   Widget buildPassword() {
-    return Container(
-      margin: EdgeInsets.only(top: 10, bottom: 10),
-      padding: EdgeInsets.only(left: 10.0, right: 10.0),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(
-            color: Colors.blue,
-          ),
-          borderRadius: BorderRadius.all(Radius.circular(30))),
-      child: TextFormField(
-          controller: passwordTextEditingController,
-          textAlign: TextAlign.start,
-          obscureText: true,
-          style: TextStyle(color: Colors.blue),
-          decoration: InputDecoration(
-            prefixIcon: Icon(
-              Icons.lock,
-              color: Colors.blue,
+    return Expanded(
+      flex: 2,
+      child: Container(
+        margin: EdgeInsets.only(top: 10, bottom: 10),
+        padding: EdgeInsets.only(left: 10.0, right: 10.0),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(
+              color: Colors.blue[300],
             ),
-            labelText: 'Password',
-            labelStyle: TextStyle(color: Colors.blue, letterSpacing: 1.0),
-            border: InputBorder.none,
-          ),
-          validator: (val) {
-            return val.length < 4 ? "Enter Password 6+ characters" : null;
-          }),
+            borderRadius: BorderRadius.all(Radius.circular(10))),
+        child: TextFormField(
+            controller: passwordTextEditingController,
+            textInputAction: TextInputAction.done,
+            textAlign: TextAlign.start,
+            obscureText: true,
+            style: TextStyle(color: Colors.blue),
+            decoration: InputDecoration(
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Colors.grey[700],
+              ),
+              labelText: 'Password',
+              labelStyle: TextStyle(color:Colors.grey[700], letterSpacing: 1.0),
+              border: InputBorder.none,
+            ),
+            validator: (val) {
+              return val.length < 4 ? "Enter Password 6+ characters" : null;
+            } ,
+          focusNode: _passwordFocus,
+          onFieldSubmitted: (term) {
+            _passwordFocus.unfocus();
+          },
+            ),
+      ),
     );
   }
 
   Widget buildKeepMeLogIn() {
-    return Container(
-      margin: EdgeInsets.all(10.0),
-      child: Row(
-        children: <Widget>[
-          Text(
-            'Keep me logged in ',
-            style:
-            TextStyle(fontWeight: FontWeight.normal, color: Colors.black),
-          ),
-          Spacer(),
-          Flexible(
-            child: Container(
-              alignment: Alignment.centerRight,
-              child: Switch(
-                value: switchState,
-                activeColor: mainColor,
-                onChanged: (bool s) {
-                  setState(() {
-                    switchState = s;
-                  });
-                },
+    return Expanded(
+      flex: 1,
+      child: Container(
+        margin: EdgeInsets.all(10.0),
+        child: Row(
+          children: <Widget>[
+            Text(
+              'Keep me logged in ',
+              style:
+              TextStyle(fontWeight: FontWeight.normal, color: Colors.black),
+            ),
+            Spacer(),
+            Flexible(
+              child: Container(
+                alignment: Alignment.centerRight,
+                child: Switch(
+                  value: switchState,
+                  activeColor: mainColor,
+                  onChanged: (bool s) {
+                    setState(() {
+                      switchState = s;
+                    });
+                  },
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 
   Widget buildLogInButton() {
     return Expanded(
+      flex: 3,
       child: GestureDetector(
         onTap: () => logInFun(),
         child: Container(
           alignment: Alignment.center,
-          margin: EdgeInsets.only(left: 10, right: 10, top: 20),
-          width: width,
-          height: height * 0.08,
+          margin: EdgeInsets.only(left: 10, right: 10, top: 60 ),
           decoration: BoxDecoration(
             color: Color(0xff1295df),
             borderRadius: BorderRadius.circular(30.0),
@@ -352,7 +390,8 @@ class _SignInState extends State<SignIn> with SingleTickerProviderStateMixin {
   logInFun() async {
     connectStatus checkConnectivity = await UtilsClass.checkConnectivity();
     if (checkConnectivity == connectStatus.connected) {
-      String macAddress = await macAddressChecker();
+      // String macAddress = await macAddressChecker();
+      String macAddress =  "00:00:00:00:00:00";
       if (formKey.currentState.validate()) {
         _bloc.add(LoginEvent(
             user: Logginer(
@@ -361,10 +400,17 @@ class _SignInState extends State<SignIn> with SingleTickerProviderStateMixin {
                 macAddress: macAddress)));
       }
     } else {
-      UtilsClass.showMyDialog(
+      scaffoldKey.currentState.showBottomSheet((widgetBuilder){
+        return Container(
+          height: 50,
+          width: double.infinity,
+          color: Colors.white,
+          child:  Center(child: Text('There is no internet connection')),);
+      });
+/*      UtilsClass.showMyDialog(
           context: context,
-          content: 'There is no internet connection',
-          type: DialogType.warning);
+          content: ,
+          type: DialogType.warning);*/
     }
   }
 
@@ -433,6 +479,11 @@ class _SignInState extends State<SignIn> with SingleTickerProviderStateMixin {
     } else {
       print('denied');
     }
+  }
+
+  _fieldFocusChange(BuildContext context, FocusNode currentFocus,FocusNode nextFocus) {
+    currentFocus.unfocus();
+    FocusScope.of(context).requestFocus(nextFocus);
   }
 
 //  Widget buildLoginUiWithOldDesign(){
